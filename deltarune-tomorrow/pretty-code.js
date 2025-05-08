284f2c61dff8e81be2f7a39915bd5ba6f00a8e23
Proto :3 <103845094+ProotTheFoxCodes@users.noreply.github.com>
2025-05-09 00:05:56 +0100

    Update pretty-code.js
    fixed

--- deltarune-tomorrow/pretty-code.js
+++ deltarune-tomorrow/pretty-code.js
@@ -2,7 +2,7 @@ const deltr = "$(countdown June 5 2025 12:00:00 AM Etc/GMT-4)";
 if (deltr.includes("month")) {
 deltr.slice(0,15) + "of";
 } else {
-  if (deltr.charAt(0) == "7"; {
+  if (deltr.charAt(0) == "7" {
     return "https://www.youtube.com/shorts/lF8nYw8EGgI";
   } else {
 deltr.slice(0,2);
