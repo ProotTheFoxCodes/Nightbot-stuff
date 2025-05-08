5e7ef591b3435a13edd18079469e358ea9a05d3d
Proto :3 <103845094+ProotTheFoxCodes@users.noreply.github.com>
2025-05-09 00:16:03 +0100

    Update pretty-code.js
    omg it was obvious the whole fucking time

--- deltarune-tomorrow/pretty-code.js
+++ deltarune-tomorrow/pretty-code.js
@@ -1,7 +1,7 @@
 const deltr = "$(countdown June 5 2025 12:00:00 AM Etc/GMT-4)";
 if (deltr.includes("month")) {
 deltr.slice(0,15) + "of";
-} else if (deltr.charAt(0) == "7" {
+} else if (deltr.charAt(0) == 7) {
     return "https://www.youtube.com/shorts/lF8nYw8EGgI";
   } else {
 deltr.slice(0,2);
