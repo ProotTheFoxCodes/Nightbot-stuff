d5e2a4d9dff97e089cef75695bcd1057d964d86b
Proto :3 <103845094+ProotTheFoxCodes@users.noreply.github.com>
2025-05-09 11:48:52 +0100

    Update pretty-code.js
    stops "kris! only a week away!" easter egg from showing when <= 1 day left

--- deltarune-tomorrow/pretty-code.js
+++ deltarune-tomorrow/pretty-code.js
@@ -1,7 +1,7 @@
 const deltr = "$(countdown June 5 2025 0:00:00 AM Etc/GMT+9)";
 if (deltr.includes("month")) {
 deltr.slice(0,15) + "of tommorows";
-} else if (deltr.charAt(0) == 7) {
+} else if ((deltr.charAt(0) == 7) && deltr.includes("day")){
     "https://www.youtube.com/shorts/lF8nYw8EGgI";
   } else if ((deltr.charAt(0) <= 3) || !(deltr.includes("day"))) {
     "$(countdown June 5 2025 0:00:00 AM Etc/GMT+9)";
